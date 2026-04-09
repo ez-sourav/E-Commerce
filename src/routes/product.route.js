6fbd231d7fcc92047c683a5903734ccb87f6a967
ez-sourav <souravb2003@gmail.com>
2026-04-09 19:33:12 +0530

    add router for product

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { protect,isAdmin, } from '../middlewares/auth.middleware.js';
-import { createProduct,getAllProducts, getProductById } from '../controllers/product.controller.js';
+import { createProduct,getAllProducts, getProductById,updateProduct, deleteProductById } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
@@ -9,5 +9,9 @@ router.post('/',protect,isAdmin,createProduct);
 router.get('/',getAllProducts);
 
 router.get('/:productId',getProductById);
+
+router.patch('/:productId',protect,isAdmin, updateProduct);
+
+router.delete('/:productId',protect,isAdmin, deleteProductById);
 
 export default router;
