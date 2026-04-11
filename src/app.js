40d6c851ca9832590ccf6e09c5da5708b5bca484
ez-sourav <souravb2003@gmail.com>
2026-04-11 19:18:24 +0530

    add order router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express"
 import cookieParser from "cookie-parser";
 import authRouters from './routes/auth.route.js'
-import testroutes from "./routes/test.route.js";
-import productrouters from './routes/product.route.js';
-import cartroutes from './routes/cart.route.js';
+import testRoutes from "./routes/test.route.js";
+import productRouters from './routes/product.route.js';
+import cartRoutes from './routes/cart.route.js';
+import orderRoutes from './routes/order.route.js';
 
 const app = express();
 app.use(express.json());
@@ -11,9 +12,10 @@ app.use(cookieParser());
 
 
 app.use('/api/auth',authRouters);
-app.use('/api/test',testroutes);
-app.use('/api/products',productrouters);
-app.use('/api/cart',cartroutes);
+app.use('/api/test',testRoutes);
+app.use('/api/products',productRouters);
+app.use('/api/cart',cartRoutes);
+app.use('/api/orders',orderRoutes);
 
 app.get('/',(req,res)=>{
     res.send("API is running...");
