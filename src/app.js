7004bb598ddc2492e04a77fe8c82dec5273a48b8
ez-sourav <souravb2003@gmail.com>
2026-04-12 12:15:39 +0530

    add swagger route

--- src/app.js
+++ src/app.js
@@ -6,10 +6,14 @@ import productRouters from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
 import orderRoutes from './routes/order.route.js';
 
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./utils/swagger.js";
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api/auth',authRouters);
 app.use('/api/test',testRoutes);
