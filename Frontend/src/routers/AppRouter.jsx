9f0c1785fdf5242d3226225754673bbcab99f752
Sourav Biswas <souravb2003@gmail.com>
2026-07-31 19:03:18 +0530

    add routes

--- Frontend/src/routers/AppRouter.jsx
+++ Frontend/src/routers/AppRouter.jsx
@@ -5,12 +5,16 @@ import Shop from '../pages/Shop'
 import ProductDetails from '../pages/ProductDetails'
 import Cart from '../pages/Cart'
 import Wishlist from '../pages/Wishlist'
+import Profile from "../pages/Profile"
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import NotFound from '../pages/NotFoundPage'
 import Checkout from '../pages/Checkout'
 import MainLayout from '../layouts/MainLayout'
 import { Toaster } from "sonner";
+import ProtectedRoute from '../components/auth/ProtectedRoute'
+import PublicRoute from "../components/auth/PublicRoute";
+import OrderConfirmation from "../pages/OrderConfirmation";
 
 const AppRouter = () => {
 
@@ -21,25 +25,67 @@ const AppRouter = () => {
                 richColors
                 position="top-center"
                 closeButton
-                duration={1400}
+                duration={1800}
                 theme="light"
             />
 
             <Routes>
+
                 <Route element={<MainLayout />}>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/shop' element={<Shop />} />
-                    <Route path='/product/:id' element={<ProductDetails />} />
-                    <Route path='/cart' element={<Cart />} />
-                    <Route path='/wishlist' element={<Wishlist />} />
-                    <Route path='/checkout' element={<Checkout />} />
+
+                    <Route path="/" element={<Home />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/product/:id" element={<ProductDetails />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/wishlist" element={<Wishlist />} />
+
+                    <Route path="/checkout"
+                        element={
+                            <ProtectedRoute>
+                                <Checkout />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/order-confirmation"
+                        element={
+                            <ProtectedRoute>
+                                <OrderConfirmation />
+                            </ProtectedRoute>
+                        }
+                    />
+
+                    <Route path="/profile"
+                        element={
+                            <ProtectedRoute>
+                                <Profile />
+                            </ProtectedRoute>
+                        }
+                    />
                 </Route>
 
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} />
+                <Route
+                    path="/login"
+                    element={
+                        <PublicRoute>
+                            <Login />
+                        </PublicRoute>
+                    }
+                />
 
-                <Route path='*' element={<NotFound />} />
+                <Route
+                    path="/register"
+                    element={
+                        <PublicRoute>
+                            <Register />
+                        </PublicRoute>
+                    }
+                />
+
+                <Route path="*" element={<NotFound />} />
+
             </Routes>
+
         </BrowserRouter>
     )
 }
