9d933fbe285d6892c391d2cb245fddeb1121f571
Sourav Biswas <souravb2003@gmail.com>
2026-08-01 15:34:05 +0530

    hide footer from some pages

--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 
-import { Outlet } from "react-router-dom"
+import { Outlet,useLocation  } from "react-router-dom"
 const MainLayout = () => {
-
+const location = useLocation();
+const hideFooter = [
+    "/cart",
+    "/checkout",
+    "/payment",
+    "/order-confirmation",
+].includes(location.pathname);
     return (
         <>
             <Navbar />
             <main className='pb-12 lg:pb-0'>
                 <Outlet />
             </main>
-            <Footer />
+           {!hideFooter && <Footer />}
         </>
     )
 }
