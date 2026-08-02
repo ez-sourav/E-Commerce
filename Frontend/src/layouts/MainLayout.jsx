3f02baff4d82b7783aac701d26e5f681d30062fd
Sourav Biswas <souravb2003@gmail.com>
2026-08-02 13:54:24 +0530

    commit

--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Footer from '../components/footer/Footer'
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
-import { Outlet,useLocation  } from "react-router-dom"
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+
 const MainLayout = () => {
-const location = useLocation();
-const hideFooter = [
-    "/cart",
-    "/checkout",
-    "/payment",
-    "/order-confirmation",
-].includes(location.pathname);
+    const location = useLocation();
+
+    const isOrderConfirmation =
+        location.pathname.startsWith("/order-confirmation");
+
+    const hideNavbar =
+        location.pathname === "/payment" ||
+        isOrderConfirmation;
+
+    const hideFooter =
+        location.pathname === "/cart" ||
+        location.pathname === "/checkout" ||
+        location.pathname === "/payment" ||
+        isOrderConfirmation;
+
     return (
         <>
-            <Navbar />
-            <main className='pb-12 lg:pb-0'>
+            {!hideNavbar && <Navbar />}
+
+            <main className="pb-12 lg:pb-0">
                 <Outlet />
             </main>
-           {!hideFooter && <Footer />}
-        </>
-    )
-}
 
-export default MainLayout
+            {!hideFooter && <Footer />}
+        </>
+    );
+};
+
+export default MainLayout;
